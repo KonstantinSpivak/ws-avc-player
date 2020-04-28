dcdf870833f566e2d227fead5d49c436f50bd7b6
root <root@ubuntu.local>
2020-04-28 11:32:59 +0000

    f

--- example/tchecker.sh
+++ example/tchecker.sh
@@ -4,24 +4,24 @@ sleep 10
 while true
 do
     frameA=$(tail ./ffmpeg.log -n 1 | sed -nr 's/.*frame=(.*)fps.*/\1/p')
-    echo "$frameA"
+  #  echo "$frameA"
     sleep 5
     frameB=$(tail ./ffmpeg.log -n 1 | sed -nr 's/.*frame=(.*)fps.*/\1/p')
-    echo "$frameB"
+  #  echo "$frameB"
 
     if [ "$frameA" = "$frameB" ]
     then
-        echo "Stream has hung"
+   #     echo "Stream has hung"
 	printf "%s - Stream has hung\n" "$(date)" >> stream.log
         pkill ffmpeg
-        echo "killed ffmpeg..."
+    #    echo "killed ffmpeg..."
 	printf "%s - Killed ffmpeg...\n" "$(date)" >> stream.log
-        echo "Waiting 5 secs"
+     #   echo "Waiting 5 secs"
         sleep 5
         bash ./ffmpeg.sh &
         echo "started ffpmeg.."
 	printf "%s - Started ffmpeg..\n" "$(date)" >> stream.log
-        echo "Waiting 15 secs"
+      #  echo "Waiting 15 secs"
         sleep 15
     else 
         echo "Stream looks ok."
